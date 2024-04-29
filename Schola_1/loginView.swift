330e9bfa168fca4bfbcae5928ba4ced8453c61bc
rodrisy <rodrigo.s.yuste@gmail.com>
2024-04-29 12:55:44 -0400

    login screen
    made a lot of progress with a owkring login screen. next: add a database to work with the validation of the logijn

--- Schola_1/loginView.swift
+++ Schola_1/loginView.swift
@@ -5,49 +5,114 @@ struct LoginView: View {
     
     @State private var backgroundColor = Color(UIColor.systemGray5) // Change the background color here
     @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
     
     var body: some View {
-        ZStack {
-            backgroundColor.edgesIgnoringSafeArea(.all)
-            VStack {
-                // welcome title
-                Text("welcome to Schola")
-                    .font(.system(size: 56, weight: .bold, design: .default))
-                    .padding()
-                // instructions
-                Text("enter your school email here. without the domain though.")
-                    .font(.system(size: 24, weight: .semibold, design: .default))
-                // text input
-                TextField("2025rsanchez", text: $username)
-                    .background(Color.black.opacity(0.5))
-                    .disableAutocorrection(true)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                
-                // submit button
-                NavigationLink(destination: ContentView3()) {
-                    Text("next bish")
+        NavigationView{
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle ()
+                    .scale (1.35)
+                    .foregroundColor(.white)
+                // open vstack
+                VStack{
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
                         .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                }
-                .buttonStyle(BorderlessButtonStyle()) // Apply ButtonStyle
-                .padding()
-                .onTapGesture {
-                    print("Button tapped!")
-                    validateUsername(username: username) // Call the validation function
-                }
+                    TextField("Username", text: $username )
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background (Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red.opacity(0.5), width: CGFloat(wrongUsername))
+                    SecureField("Password", text: $password )
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background (Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red.opacity(0.5), width: CGFloat(wrongPassword))
+                    Button(action: {
+                        // authenticate the user
+                        authenticateUser(username: username, password: password)
+                    }) {
+                        Text("login")
+                            .foregroundColor(.white)
+                            .frame(width: 300, height: 50) // Adjust button frame to cover the entire area
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .padding() // Add padding around the button
+                    }
+                    //navlink to other screen
+                    NavigationLink("Work Folder", isActive: $showingLoginScreen) {
+                        ContentView3()
+                    }
+                }// close Vstack
+            }// close Ztack
+            .navigationBarHidden(true)
+        }// close navview
+        
+    }// close body
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "2025rsanchez" {
+            wrongUsername = 0
+            if password == "1234"{
+                wrongPassword = 0
+                showingLoginScreen = true
+            }else{
+                wrongPassword = 3
             }
+        }else{
+            wrongUsername = 3
         }
     }
     
-    private func validateUsername(username: String) {
-        print("Validation: \(username)")
-        // Add your validation logic here
+    struct LoginView_Previews: PreviewProvider {
+        static var previews: some View {
+            LoginView()
+        }
     }
-}
+    
+}// close loginView
 
-#Preview{
-    LoginView()
-}
+
+//ZStack {
+//    backgroundColor.edgesIgnoringSafeArea(.all)
+//    VStack {
+//        // welcome title
+//        Text("welcome to Schola")
+//            .font(.system(size: 56, weight: .bold, design: .default))
+//            .padding()
+//        // instructions
+//        Text("enter your school email here. without the domain though.")
+//            .font(.system(size: 24, weight: .semibold, design: .default))
+//        // text input
+//        TextField("2025rsanchez", text: $username)
+//            .background(Color.black.opacity(0.5))
+//            .disableAutocorrection(true)
+//            .textFieldStyle(RoundedBorderTextFieldStyle())
+//            .padding()
+//        
+//        // submit button
+//        NavigationLink(destination: ContentView3()) {
+//            Text("next bish")
+//                .padding()
+//                .background(Color.blue)
+//                .foregroundColor(.white)
+//                .cornerRadius(8)
+//        }
+//        .buttonStyle(BorderlessButtonStyle()) // Apply ButtonStyle
+//        .padding()
+//        .onTapGesture {
+//            print("Button tapped!")
+//            validateUsername(username: username) // Call the validation function
+//        }
+//    }
+//}
