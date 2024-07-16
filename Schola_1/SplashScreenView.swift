cab375158618a3409bf5ff3db0b6f2fa1f39b810
rodrisy <rodrigo.s.yuste@gmail.com>
2024-07-16 21:29:13 +0200

    Update SplashScreenView.swift

--- Schola_1/SplashScreenView.swift
+++ Schola_1/SplashScreenView.swift
@@ -13,26 +13,33 @@ struct SplashScreenView: View {
     @State private var opacity = 0.5
     
     var body: some View {
-        VStack{
+        
+        if isActive {
+            LoginView()
+        } else {
+        // // //
             VStack{
-                Image("schola_elephant copy")
-                            .resizable()
-                            .frame(width: 150, height: 150) // Adjust the width and height as needed
-                Spacer().frame(height: 0)
-                Text("Schola").font(.largeTitle.bold())
-            }
-            .scaleEffect(size)
-            .opacity(opacity)
-            .onAppear{
-                withAnimation(.easeIn(duration: 1.2)) {
-                    self.size = 0.9
-                    self.opacity = 1.0
+                VStack{
+                    Image("schola_elephant copy")
+                                .resizable()
+                                .frame(width: 150, height: 150) // Adjust the width and height as needed
+                    Spacer().frame(height: 0)
+                    Text("Schola").font(.largeTitle.bold())
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear{
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }.onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    self.isActive = true
                 }
             }
-        }.onAppear{
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                self.isActive = true
-            }
+        // // //
         }
     }
 }
